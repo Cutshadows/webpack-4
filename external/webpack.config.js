ddde2c09b3001578983d603e0929da4417dae049
Cutshadows <ramnstein_1749_@hotmail.com>
2020-01-19 05:50:16 -0300

    first commit

---
+++ external/webpack.config.js
@@ -0,0 +1,15 @@
+const path=require('path')
+
+module.exports={
+    entry:path.resolve(__dirname, 'index.js'),
+    mode:'development',
+    output:{
+        //path:path.resolve('output', 'js')//ruta que se puede poner
+        //path:path.resolve(__dirname)//ruta que se puede poner
+        path:path.resolve(__dirname, './dist'),//definir otra ruta para guardar
+        filename:'bundle.js'
+    }
+}
+//common js
+
+//dist (compilados)es una convencion de desarrollo para poner ahi en esa carpeta todo lo relacionado a webpack
